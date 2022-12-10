e6b3349a2dda251f78a463acf0d2ea7ff255b730
Guilherme Galesso <75454472+guilhermegalesso@users.noreply.github.com>
2022-12-10 16:52:44 -0300

    Project created
    Project base created with Spring Initializ.

---
+++ src/test/java/com/guilhermegalesso/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermegalesso.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
